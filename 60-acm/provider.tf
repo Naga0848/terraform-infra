09fa9970c0ecf464f539f16f3b768d2c10c95a04
Nagashankar <nagashankar0848@gmail.com>
2025-12-18 18:29:46 +0530

    jenkins

--- 60-acm/provider.tf
+++ 60-acm/provider.tf
@@ -2,16 +2,8 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.98.0"
+      version = "6.0.0"
     }
-  }
-
-  backend "s3" {
-    bucket = "latest-84s-remote-state-dev"
-    key    = "roboshop-dev-acm"
-    region = "us-east-1"
-    encrypt        = true
-    use_lockfile = true
   }
 }
 
--- 80-eks/variables.tf
+++ 80-eks/variables.tf
@@ -7,9 +7,9 @@ variable "environment" {
 }
 
 variable "zone_id" {
-    default = "Z032558618100M4EJX8X4"
+    default = "Z0493708AWY0JR1IZP1L"
 }
 
 variable "zone_name" {
-    default = "daws84s.site"
+    default = "njanapati.site"
 }
