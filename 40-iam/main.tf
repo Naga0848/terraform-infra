4d1628cf5ff10c0c60a90649f15defd2c59fea9c
Nagashankar <nagashankar0848@gmail.com>
2025-12-24 09:51:00 +0530

    jenkins

--- 40-iam/main.tf
+++ 40-iam/main.tf
@@ -4,3 +4,7 @@ resource "aws_iam_policy" "alb" {
   policy      = file("${path.module}/iam-policy.json")
 }
 
+resource "aws_iam_role_policy_attachment" "terraform_admin_eks_cluster" {  // IAM Role for Bastion/EKS Access
+  role       = aws_iam_role.terraform_admin.name  # Adjust to your role resource name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+} 
