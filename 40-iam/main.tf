478cb19e70861c5d810976f4a7d5cd8c6bc0d5eb
Nagashankar <nagashankar0848@gmail.com>
2025-12-28 21:12:47 +0530

    jenkins

--- 40-iam/main.tf
+++ 40-iam/main.tf
@@ -3,8 +3,26 @@ resource "aws_iam_policy" "alb" {
   description = "ALB Controller permissions"
   policy      = file("${path.module}/iam-policy.json")
 }
+# This must exist
+resource "aws_iam_role" "terraform_admin" {
+  name = "TerraformAdmin"
 
-resource "aws_iam_role_policy_attachment" "terraform_admin_eks_cluster" {  // IAM Role for Bastion/EKS Access
-  role       = aws_iam_role.terraform_admin.name  # Adjust to your role resource name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Add this new attachment
+resource "aws_iam_role_policy_attachment" "terraform_admin_eks_cluster" { // IAM Role for Bastion/EKS Access
+  role       = aws_iam_role.terraform_admin.name   # This refers to the role above
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-} 
+}
